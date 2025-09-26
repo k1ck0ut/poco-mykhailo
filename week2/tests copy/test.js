cdc21c794bd9fc0800caab12e7495af6e0f84780
k1ck0ut <mykhailo.bolkun@powercoders.org>
2025-09-26 15:02:37 +0200

    lol

--- week2/tests copy/test.js
+++ week2/tests copy/test.js
@@ -1,8 +1,12 @@
-for (i = 12; i <= 114; i++){
-    if (i %2 != 0){
+for (i = 50; i > -15; i--){
+    if (i % 3 == 0 && i < 50 && i > 20) {
+     console.log(i, "from 50-20 multiple of 3")
+    }
+    else if (i %2 != 0){
      console.log(i, "ODD")
     }
     else if ( i%2 == 0){
      console.log(i, "EVEN")
     }
+
 }
