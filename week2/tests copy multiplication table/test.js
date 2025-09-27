97f7354bfdbf8db570d1725c3a0e14066c2ccfdb
k1ck0ut <mykhailo.bolkun@powercoders.org>
2025-09-27 11:21:25 +0200

    table

---
+++ week2/tests copy multiplication table/test.js
@@ -0,0 +1,13 @@
+for (i = 1; i <= 10; i++){
+    let one = 1*i;
+    let two = 2*i;
+    let three = 3*i;
+    let four = 4*i;
+    let five = 5*i;
+    let six = 6*i;
+    let seven = 7*i;
+    let eight = 8*i;
+    let nine = 9*i;
+    let ten = 10*i;
+console.log(one, two, three, four, five, six, seven, eight, nine, ten)
+}
